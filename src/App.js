5be6f5414b5185a109662dae48e78b54aa24dad1
bharat2913 <bs987143@gmail.com>
2021-05-13 14:29:02 +0530

    compeleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Movie from './compoments/Movie';
 
+const MOVIE_API =
+  'https://api.themoviedb.org/3/movie/popular?api_key=8752accf3bdf50d86a56668c371ce63e&page=1';
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=8752accf3bdf50d86a56668c371ce63e&query=';
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+
+  useEffect(() => {
+    getMovies(MOVIE_API);
+  }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h2>Movie Finder</h2>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className='search'
+            type='text'
+            placeholder='Search..'
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
-    </div>
+      <div className='movie-container'>
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
